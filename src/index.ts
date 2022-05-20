f3088a8561a5c235b1acc4e618b6655426e4dd5e
rgasali <96292535+rgasali@users.noreply.github.com>
2022-05-20 10:08:40 -0300

    ejercicio2testeado

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,26 @@ let divMayor = <HTMLDivElement>document.getElementById("divMayor");
 let divMedia = <HTMLDivElement>document.getElementById("divMedia");
 let arreglo: number[] = new Array();
 let valorMedio = 0;
+let testearNumero = new RegExp("^[0-9]*$");
+let largoVector: number;
+let valorIngresado: number;
 
 btnCargaVector.addEventListener("click", () => {
-  arreglo.length = Number(prompt("ingrese el tamaño del vector"));
+  largoVector = Number(prompt("ingrese el tamaño del vector"));
+  while (!testearNumero.test(largoVector)) {
+    largoVector = Number(prompt("el valor ingresado debe ser un numero"));
+  }
+  arreglo.length = largoVector;
 });
 
 btnCargaValores.addEventListener("click", () => {
   for (let i = 0; i < arreglo.length; i++) {
-    arreglo[i] = Number(prompt("ingrese un valor al vector"));
+    valorIngresado = Number(prompt("ingrese un valor al vector"));
+    while (!testearNumero.test(valorIngresado)) {
+      valorIngresado = Number(prompt("el valor ingresado debe ser un numero"));
+    }
+    arreglo[i] = valorIngresado;
+
     valorMedio = valorMedio + Number(arreglo[i]);
   }
 
